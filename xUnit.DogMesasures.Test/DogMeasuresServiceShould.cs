7f8bdae5736c05e171b3c56aae132477b629bf79
John López <johnalbh@gmail.com>
2020-12-21 12:20:35 +0100

    [Test2 ] - First Tes with xUnit.

--- xUnit.DogMesasures.Test/DogMeasuresServiceShould.cs
+++ xUnit.DogMesasures.Test/DogMeasuresServiceShould.cs
@@ -1,4 +1,5 @@
 using System;
+using Linkedin.DogMeasures.Repositories;
 using Linkedin.DogMeasures.Services;
 using Xunit;
 
@@ -7,5 +8,18 @@ namespace xUnit.DogMesasures.Test
     public class DogMeasuresServiceShould
     {
         private DogMeasuresService _dogMeasuresService;
+
+        public DogMeasuresServiceShould()
+        {
+            _dogMeasuresService = new DogMeasuresService(new DogMeasuresRepository());
+        }
+
+        [Fact]
+        public void ThrowArgumenNullException_IfBreedOsNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => _dogMeasuresService.CheckDogIdealWeight(null, 0));
+        }
+
+
     }
 }
