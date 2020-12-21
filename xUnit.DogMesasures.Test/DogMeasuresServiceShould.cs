7edcc265f00d62ff29c885568db7293da21304f4
John López <johnalbh@gmail.com>
2020-12-21 12:26:16 +0100

    [Test 2 ] - Other Test xUnit

--- xUnit.DogMesasures.Test/DogMeasuresServiceShould.cs
+++ xUnit.DogMesasures.Test/DogMeasuresServiceShould.cs
@@ -1,4 +1,6 @@
 using System;
+using Linkedin.DogMeasures.Exceptions;
+using Linkedin.DogMeasures.Models;
 using Linkedin.DogMeasures.Repositories;
 using Linkedin.DogMeasures.Services;
 using Xunit;
@@ -22,12 +24,22 @@ namespace xUnit.DogMesasures.Test
         [Fact]
         public void ThrowsBreedNotFoundException_IfBreedIsSamoyedo()
         {
-            Assert.Throws<ArgumentNullException>(() => _dogMeasuresService.CheckDogIdealWeight("samoyedo", 20));
+            Assert.Throws<BreedNotFoundException>(() => _dogMeasuresService.CheckDogIdealWeight("samoyedo", 20));
         }
         [Fact]
         public void ThrowsArgumentOutOfRangeException_IfBreedOsNull()
         {
-            Assert.Throws<ArgumentNullException>(() => _dogMeasuresService.CheckDogIdealWeight("bóxer", -5));
+            Assert.Throws<ArgumentOutOfRangeException>(() => _dogMeasuresService.CheckDogIdealWeight("bóxer", -5));
+        }
+
+        [InlineData((20))]
+        [Theory]
+        public void DogIsIdealWeight(int Weight)
+        {
+            var result = _dogMeasuresService.CheckDogIdealWeight("Labrador retriever", Weight);
+            Assert.True(result.DeviationType == DogWeightInfo.WeightDeviationType.InRange);
+            Assert.Equal(0, result.WeightDeviation);
+
         }
 
     }
