66e704c0bb31be5d14ad94d3c7c93c73ef817bc1
John López <johnalbh@gmail.com>
2020-12-21 12:16:36 +0100

    [Test2] - MsTest implementation.

---
+++ DogMeasures.Tests/DogMeasuresServiceShould.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Linkedin.DogMeasures.Exceptions;
+using Linkedin.DogMeasures.Models;
+using Linkedin.DogMeasures.Repositories;
+using Linkedin.DogMeasures.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DogMeasures.Tests
+{
+    [TestClass]
+    public class DogMeasuresServiceShould
+    {
+        private static DogMeasuresService _dogMeasuresService;
+
+        [ClassInitialize]
+        public static void Setup(TestContext context)
+        {
+            _dogMeasuresService = new DogMeasuresService(new DogMeasuresRepository());
+        }
+
+        [ExpectedException(typeof(ArgumentNullException))]
+        [TestMethod]
+        public void ThrowsArgumentNullException_IfBreedIsNull()
+        {
+            _dogMeasuresService.CheckDogIdealWeight(null, 0);
+        }
+
+        [ExpectedException(typeof(BreedNotFoundException))]
+        [TestMethod]
+        public void ThrowsBreedNotFoundException_IfBreedIsSamoyedo()
+        {
+            _dogMeasuresService.CheckDogIdealWeight("samoyedo", 20);
+        }
+
+        [DataTestMethod]
+        [DataRow("Labrador Retriever", 20)]
+        [DataRow("Labrador Retriever", 22)]
+        [DataRow("Labrador Retriever", 20)]
+
+        public void DogIsIdealWeight_IfBreedIsLabradorAndWeightInRange20and40(string breed, int weight)
+        {
+            var result = _dogMeasuresService.CheckDogIdealWeight(breed, weight);
+            Assert.IsTrue(result.DeviationType == DogWeightInfo.WeightDeviationType.InRange);
+            Assert.AreEqual(0, result.WeightDeviation);
+        }
+
+
+    }
+}
---
+++ xUnit.DogMesasures.Test/DogMeasuresServiceShould.cs
@@ -0,0 +1,11 @@
+using System;
+using Linkedin.DogMeasures.Services;
+using Xunit;
+
+namespace xUnit.DogMesasures.Test
+{
+    public class DogMeasuresServiceShould
+    {
+        private DogMeasuresService _dogMeasuresService;
+    }
+}
